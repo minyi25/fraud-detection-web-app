af00c25be641a674a084a667ee2e324552d912da
Foo Kai Wen <kelvin_foo_13vin@hotmail.com>
2024-12-03 08:24:14 +0800

    test again

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def test():
-    return jsonify(True)
+    return render_template('index.html')
