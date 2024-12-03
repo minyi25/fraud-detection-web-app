20aaa2b2ee67a756e700ffe2da3176fed382d449
Foo Kai Wen <kelvin_foo_13vin@hotmail.com>
2024-12-03 09:09:22 +0800

    added another route

--- api/index.py
+++ api/index.py
@@ -3,5 +3,9 @@ from flask import Flask, request, jsonify, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-def test():
+def root():
     return render_template('index.html')
+
+@app.route('/api')
+def api():
+    return jsonify([True])
